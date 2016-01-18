a72d8ee114468575f4ed552f7f9aa3e27d531208
Conor Hastings <hastings.conorm@gmail.com>
2016-01-18 13:17:08 -0500

    convert test display component to be stateless function component

--- src/display-test.js
+++ src/display-test.js
@@ -5,41 +5,38 @@ const onXClick = (e, close) => {
   close();
 };
 
-class DisplayTest extends React.Component {
-  render() {
-    const { getTest, shouldShowTest, onKeyPress, hideTest } = this.props;
-    if (!shouldShowTest()) {
-      return null;
-    }
-    const style = {
-      position: 'absolute',
-      width: 500,
-      height: 600,
-      right: 25,
-      bottom: 80,
-      backgroundColor: 'gainsboro',
-      color: 'black',
-      boxShadow: '2px 2px 5px #888888',
-      overflowY: 'auto'
-    };
-    const xStyle = {
-      float: 'right',
-      fontSize: 22,
-      fontWeight: 700,
-      marginRight: 5,
-      cursor: 'pointer'
-    };
-    return (
-      <div style={style}>
-        <span style={xStyle} onClick={e => onXClick(e, hideTest)}>X</span>
-        <div contentEditable={true} onKeyUp={onKeyPress} style={{outline: 'none'}}>
-          <pre><code>
-            <span>{getTest()}</span>
-          </code></pre>
-        </div>
-      </div>
-    );
+const DisplayTest = ({ getTest , shouldShowTest, onKeyPress, hideTest }) => {
+  if (!shouldShowTest()) {
+    return <noscript />;
   }
+  const style = {
+    position: 'absolute',
+    width: 500,
+    height: 600,
+    right: 25,
+    bottom: 80,
+    backgroundColor: 'gainsboro',
+    color: 'black',
+    boxShadow: '2px 2px 5px #888888',
+    overflowY: 'auto'
+  };
+  const xStyle = {
+    float: 'right',
+    fontSize: 22,
+    fontWeight: 700,
+    marginRight: 5,
+    cursor: 'pointer'
+  };
+  return (
+    <div style={style}>
+      <span style={xStyle} onClick={e => onXClick(e, hideTest)}>X</span>
+      <div contentEditable={true} onKeyUp={onKeyPress} style={{outline: 'none'}}>
+        <pre><code>
+          <span>{getTest()}</span>
+        </code></pre>
+      </div>
+    </div>
+  );
 }
 
 export default DisplayTest;
