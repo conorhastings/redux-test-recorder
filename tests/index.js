7b330eb60b02adbbcaca3103e6adda4d3d8220af
Conor Hastings <hastings.conorm@gmail.com>
2016-01-26 19:51:57 -0500

    add test case showing that recording slice of state/testing single reducer works

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxRecord from '../dist';
 
 const increment = () => {
@@ -35,3 +35,41 @@ store.dispatch(increment());
 store.dispatch(increment());
 store.dispatch(decrement());
 eval(record.props.getTest());
+
+const initState2 = {
+  add: 0,
+  subtract: 0
+}
+const add = (state = initState2.add, { type, payload }) => {
+  let newState;
+  switch (type) {
+    case 'INCREMENT':
+      newState = state + 1;
+      break;
+    default:
+      newState = state;
+  }
+  return newState;
+}
+
+const subtract = (state = initState2.subtract, { type, payload }) => {
+  let newState;
+  switch (type) {
+    case 'DECREMENT':
+      newState = state - 1;
+      break;
+    default:
+      newState = state;
+  }
+  return newState;
+}
+const combined = combineReducers({add, subtract});
+const record2 = reduxRecord({reducer: add, stateKey: 'add', actionSubset: ['INCREMENT']});
+const createStoreWithMiddleware2 = applyMiddleware(record2.middleware)(createStore);
+const store2 = createStoreWithMiddleware2(combined);
+record2.props.startRecord();
+store2.dispatch(increment());
+store2.dispatch(increment());
+store2.dispatch(increment());
+store2.dispatch(decrement());
+eval(record2.props.getTest());
