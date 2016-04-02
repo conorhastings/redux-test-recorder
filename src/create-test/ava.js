c4097a9fb0a91a16ddc54c433c9811a8882701ef
Conor Hastings <hastings.conorm@gmail.com>
2016-04-02 11:30:49 -0400

    update ava test creation

--- src/create-test/ava.js
+++ src/create-test/ava.js
@@ -1,17 +1,17 @@
- export default function createAvaTest({state, actions, imports, reducer, equalityFunction}) {
-   return (`var test = require('ava');
- ${imports}
- ${reducer}
- var equality = ${equalityFunction};
+export default function createAvaTest({state, actions, imports, reducer, equalityFunction}) {
+  const asserts = actions.map((action, index) => {
+    return `test('${action.action.type} (action index ${index}) should correctly update state', function(t) {
+    var action = actions[${index}];
+    var result = reducer(state, action.action);
+    state = result;
+    t.ok(equality(result, action.nextState));
+  });`}).join('\n\n');
+  return (`var test = require('ava');
+${imports}
+${reducer}
+var equality = ${equalityFunction};
+var actions = ${JSON.stringify(actions, null, 2)};
+var state = ${state};
 
- test('expected state returned for each action', function(t) {
-   var state = ${state};
-   var actions = ${JSON.stringify(actions, null, 2)};
-   actions.forEach(function(action, index) {
-     var result = reducer(state, action.action);
-     state = result;
-     t.ok(equality(result, action.nextState), action.action.type + '(action index ' + index +') correctly updated state');
-   });
- });`
-   );
- }
+${asserts}`);
+}
