97a97a4777c395ad3e2f0c72e34ef7c6db620748
Pawel Chojnacki <ext.pchojnacki@riotgames.com>
2016-10-11 15:53:32 +0200

    local version of es6 tests

--- src/create-test/mocha-chai-es6.js
+++ src/create-test/mocha-chai-es6.js
@@ -1,22 +1,25 @@
 export default function createMochaChaiES6Test ({actions, imports, reducer, equalityFunction}) {
   const its = actions.map((action, index) => {
-    console.log(action.action.type)
     if (action.action.type === undefined) {
       return ''
     }
     return `it('${action.action.type} should correctly update state', function () {
   const action = actions[${index}]
   const result = reducer(action.prevState, action.action)
-  expect(result).to.deep.equal(action.nextState)
+  const cleanedResult = JSON.parse(JSON.stringify(result))
+  expect(equality(cleanedResult, action.nextState)).to.be.true
 })
 `
   }).join('\n')
   return (`/* eslint-env mocha */
 
 import { expect } from 'chai'
+import _ from 'lodash'
 
 ${imports}
 ${reducer}
+
+const equality = ${equalityFunction}
 
 describe('redux reducer test', function () {
   let actions
@@ -30,5 +33,6 @@ describe('redux reducer test', function () {
   })
 
 ${its}
+
 })`.replace(/\"/g, '\''))
 }
